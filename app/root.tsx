f99e44816cd30fe4953daf0e7a5a38d7931a4788
Matthew Yasul <yasulmatthew@gmail.com>
2023-01-13 09:59:03 +0800

    Display journal calendar

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,42 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts
+} from "@remix-run/react"
+
+import styles from './tailwind.css'
+
+export const links: LinksFunction = () => ([
+    { rel: "stylesheet", href: styles }
+])
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+})
 
-export default function App() {
-  return (
+const Document = ({ children }: { children: React.ReactNode }) => (
     <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+        <head>
+            <Meta />
+            <Links />
+        </head>
+        <body>
+            {children}
+            <Scripts />
+            <LiveReload />
+        </body>
     </html>
-  );
-}
+)
+
+const Root = () => (
+    <Document>
+        <Outlet />
+    </Document>
+)
+
+export default Root
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,110 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import type { LinksFunction } from '@remix-run/node'
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import stylesUrl from "~/styles/index.css"
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: stylesUrl,
+        },
+    ]
+}
+
+dayjs.extend(weekOfYear)
+
+export default function Index () {
+    const generateCalendarSvg = () => {
+        const firstDayOfCurrentYear = dayjs(`${dayjs().year()}-01-01T00:00:00Z`)
+        const firstDayOfNextYear = firstDayOfCurrentYear.add(1, 'year')
+
+        let currentDay = firstDayOfCurrentYear
+        let currentMonth = currentDay.month()
+
+        const calendarGroups = []
+        const monthLabels = [
+            <text x="14" y="-7" className="ContributionCalendar-label" key={currentMonth}>
+                {currentDay.format('MMM')}
+            </text>
+        ]
+
+        while (currentDay.isBefore(firstDayOfNextYear)) {
+            const currentWeek = currentDay.week()
+            const days = []
+            const groupXPosition = calendarGroups.length * 14
+            const dayXPosition = 14 - calendarGroups.length
+
+            while (
+                currentDay.week() === currentWeek &&
+                currentDay.isBefore(firstDayOfNextYear)
+            ) {
+                if (currentDay.month() !== currentMonth) {
+                    monthLabels.push(
+                        <text
+                            className="ContributionCalendar-label"
+                            y="-7"
+                            x={groupXPosition + dayXPosition}
+                            key={currentMonth}
+                        >
+                            {currentDay.format('MMM')}
+                        </text>
+                    )
+                    currentMonth = currentDay.month()
+                }
+
+                const dayYPosition = currentDay.day() * 13
+
+                days.push(
+                    <rect
+                        width="10"
+                        height="10"
+                        x={dayXPosition}
+                        y={dayYPosition}
+                        className="ContributionCalendar-day"
+                        data-date={currentDay.format('DD-MM-YYYY')}
+                        data-level="0"
+                        rx="2"
+                        ry="2"
+                        key={currentDay.valueOf()}
+                    >
+                        {currentDay.toString()}
+                    </rect>
+                )
+
+                currentDay = currentDay.add(1, 'day')
+            }
+
+            const calendarGroup = (
+                <g transform={`translate(${groupXPosition}, 0)`}>
+                    {...days}
+                </g>
+            )
+
+            calendarGroups.push(calendarGroup)
+        }
+
+        return (
+            <g transform="translate(15, 20)">
+                {...calendarGroups}
+                {...monthLabels}
+
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="8" style={{ display: 'none' }}>Sun</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="22">Mon</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="32" style={{ display: 'none' }}>Tue</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="48">Wed</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="57" style={{ display: 'none' }}>Thu</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="73">Fri</text>
+                <text text-anchor="start" className="ContributionCalendar-label" dx="-15" dy="81" style={{ display: 'none' }}>Sat</text>
+            </g>
+        )
+    }
+
+    return (
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }} className='m-3'>
+            <svg width="717" height="112" className="js-calendar-graph-svg">
+                {generateCalendarSvg()}
+            </svg>
+        </div>
+    )
 }
